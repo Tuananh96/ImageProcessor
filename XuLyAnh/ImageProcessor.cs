a9977b5a89e2f821fedb2ad76a787f9a6a2f2ee1
dNG1216 <duongnguyen1216@gmail.com>
2017-02-19 18:31:06 +0700

    Them nut save file, van chua xong Tich chap anh :(

--- XuLyAnh/ImageProcessor.cs
+++ XuLyAnh/ImageProcessor.cs
@@ -53,14 +53,12 @@ namespace XuLyAnh
         public static Bitmap TichChap(Bitmap img, int [,] boLoc, int m, int n)
         {
             Bitmap newImg = new Bitmap(img);
-            //int x, y;
             Color color;
-            Byte value;
+            Byte value=0;
             for (int x = 0; x < newImg.Width; x++)
             {
                 for (int y = 0; y < newImg.Height; y++)
                 {
-                    
                     for (int i = 0; i < n; i++)
                     {
                         for (int j = 0; j < m; j++)
@@ -72,9 +70,10 @@ namespace XuLyAnh
                             if (pix1 > newImg.Width) pix1 = newImg.Width;
                             if (pix2 > newImg.Height) pix2 = newImg.Height;
                             color = newImg.GetPixel(pix1, pix2);
-                            value = Convert.ToByte(boLoc[i, j] * (color.R + color.G + color.B));
+                            value = Convert.ToByte(boLoc[i, j] * (color.R + color.G + color.B)/3);
                         }
                     }
+                    newImg.SetPixel(x, y, Color.FromArgb(value, value, value));
                 }
             }
 
